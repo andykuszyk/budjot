033f9953791ac9cc201d220f0ca0357feb828902
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-02 11:36:45 +0100

    First draft of deployment

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,3 +2,6 @@
 commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
 docker build -t andykuszyk/budjot:$commit .
 docker push andykuszyk/budjot:$commit
+ssh $douser@$doip "docker stop `docker container ls | sed 's/\([a-z0-9]\+\)\s\+.*/\1/'`"
+ssh $douser@$doip "echo $commit >> commit"
+ssh $douser@$doip "docker run -d -p 80:8080 andykuszyk/budjot:`cat commit`"
