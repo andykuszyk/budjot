91b91897a7082c868f9108f4374c71a8ae03de7e
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-03 19:42:27 +0100

    First experimentation with login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
+    onSignIn(googleUser: any) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.   
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
 }
