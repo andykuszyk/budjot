8bb03f058d65f1e19737457078dc6a894779078f
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-03 14:57:49 +0100

    Made commit path relative to home

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -4,4 +4,4 @@ docker build -t andykuszyk/budjot:$commit .
 docker push andykuszyk/budjot:$commit
 ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "if [[ `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\).*/\1/' | wc -m` != 0 ]]; then docker stop `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\)\s\+.*/\1/'`;fi"
 ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "echo $commit > commit"
-ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "docker run -d -p 80:8080 andykuszyk/budjot:`cat ./commit`"
+ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "docker run -d -p 80:8080 andykuszyk/budjot:`cat ~/commit`"
