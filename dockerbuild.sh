9f13fffb28819f42d521a066181c99332b307ab7
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-03 16:10:33 +0100

    Enclosed remote script in single quotes

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,6 +5,6 @@ docker build -t andykuszyk/budjot:$commit .
 echo "Pushing docker container..."
 docker push andykuszyk/budjot:$commit
 echo "Stopping any running containers..."
-ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "if [[ `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\).*/\1/' | wc -m` != 0 ]]; then docker stop `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\)\s\+.*/\1/'`; else echo 'no running containers found'; fi"
+ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip 'if [[ `docker container ls | sed "/CONT/d;s/\([a-z0-9]\+\).*/\1/" | wc -m` != 0 ]]; then docker stop `docker container ls | sed "/CONT/d;s/\([a-z0-9]\+\)\s\+.*/\1/"`; else echo "no running containers found"; fi'
 echo "Attempting to start new container..."
 ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "docker run -d -p 80:8080 andykuszyk/budjot:`echo $commit`"
