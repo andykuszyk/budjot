70fc0e6566a033426e2028057b3699d0b803ba18
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-03 16:04:04 +0100

    Added debug statements to build script

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
+echo "Building docker container..."
 docker build -t andykuszyk/budjot:$commit .
+echo "Pushing docker container..."
 docker push andykuszyk/budjot:$commit
-ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "if [[ `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\).*/\1/' | wc -m` != 0 ]]; then docker stop `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\)\s\+.*/\1/'`;fi"
+echo "Stopping any running containers..."
+ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "if [[ `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\).*/\1/' | wc -m` != 0 ]]; then docker stop `docker container ls | sed '/CONT/d;s/\([a-z0-9]\+\)\s\+.*/\1/'`; else echo 'no running containers found'; fi"
+echo "Attempting to start new container..."
 ssh -o "StrictHostKeyChecking no" -i id_rsa $douser@$doip "docker run -d -p 80:8080 andykuszyk/budjot:`echo $commit`"
