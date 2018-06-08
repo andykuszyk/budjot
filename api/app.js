acad66195fa1deb69b55260ed0fe26cba88baea9
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-08 22:26:21 +0100

    wip on budjot crud routes

--- api/app.js
+++ api/app.js
@@ -6,54 +6,86 @@ const mongo = require('./mongo');
 
 app.get('/jots', (req, res) => {
     // get the list of jots for the user in the auth header
+    var id = await google.verify(req.get('Authorization'));
+    if(id == null) {
+        return res.status(401).send();
+    }
+    mongo.budjot().find({ userId: id }, function(err, budjots) {
+        if(err) {
+            return res.status(500).send(err);
+        }
+        var returnBudjots = [];
+        for(var budjot of budjots) {
+            returnBudjots.push(mongo.toBudjotJson(budjot));
+        }
+        res.status(200).send(returnBudjots);
+    });
 })
 
 app.get('/jots/{id}', (req, res) => {
     // get the jot specified by the given id, provided it is for the user
     // in the auth header
     console.log(req)
+    var id = await google.verify(req.get('Authorization'));
 })
 
 app.post('/jots', (req, res) => {
     // create a jot for the user in the auth header
     console.log(req)
+    var id = await google.verify(req.get('Authorization'));
+    if(id == null) {
+        return res.status(401).send();
+    }
+    mongo.budjot().find({ name: req.body.name }, function(err, budjots) {
+        if(err) {
+            return res.status(500).send();
+        }
+        if(budjots.length > 0) {
+            return res.status(405).send();
+        }
+        mongo.fromBudjotJson(req.body).save(function(err, budjot) {
+            if(err) {
+                return res.status(500).send(err);
+            }
+            res.status(201).send(mongo.toBudjotJson(budjot));
+        });
+    });
 })
 
 app.put('/jots/{id}', (req, res) => {
     // update the jot with the given id, provided it exists for the user in the auth header
     console.log(req)
+    var id = await google.verify(req.get('Authorization'));
 })
 
 app.post('/users', async (req, res) => {
     // creates a new user if one doesn't already exist for the auth header
-    var idToken = req.get('Authorization');
-    var id = await google.verify(idToken);
+    var id = await google.verify(req.get('Authorization'));
     if(id == null) {
-        res.status(401).send();
-    } else {
-        mongo.user().findOne({ id: id }, function (err, user) {
-            if(user == null) {
-                console.log('No user found for idToken, so creating a new one');
-                var user = new mongo.user()({ id: id, created: Date.now(), lastLogin: Date.now() });
-                user.save(function (err, u) {
-                    if(err) {
-                        res.status(500).send(err);
-                    } else {
-                        res.status(201).send();
-                    }
-                });
-            } else {
-                user.lastLogin = Date.now();
-                user.save(function (err, u) {
-                    if(err) {
-                        res.status(500).send(err);
-                    } else {
-                        res.status(204).send();
-                    }
-                });
-            }
-        });
+        return res.status(401).send();
     }
+    mongo.user().findOne({ id: id }, function (err, user) {
+        if(user == null) {
+            console.log('No user found for idToken, so creating a new one');
+            var user = new mongo.user()({ id: id, created: Date.now(), lastLogin: Date.now() });
+            user.save(function (err, u) {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(201).send();
+                }
+            });
+        } else {
+            user.lastLogin = Date.now();
+            user.save(function (err, u) {
+                if(err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send();
+                }
+            });
+        }
+    });
 })
 
 app.use(express.static('dist/budjot'));
--- api/mongo.js
+++ api/mongo.js
@@ -10,9 +10,44 @@ var userSchema = new mongoose.Schema({
     lastLogin: 'date'
 });
 
+var budjotEntrySchema = new mongoose.Schema({
+    name: 'string',
+    amount: 'number',
+    paid: 'boolean'
+});
+
+var budjotSchema = new mongoose.Schema({
+    name: { type: 'string', index: true },
+    userId: { type: 'string', index: true },
+    income: 'number',
+    entries: [budjotEntrySchema]
+});
+
 module.exports = {
     user: function() {
        return mongoose.model('user', userSchema);
+    },
+    budjot: function() {
+        return mongoose.model('budjot', budjotSchema);
+    },
+    toBudjotJson: function(budjot) {
+        var budjotEntries = [];
+        for(var entry of budjot.entries) {
+            budjotEntries.push({
+                "name": entry.name,
+                "amount": entry.amount,
+                "paid": entry.paid
+            });
+        }
+        return {
+            "id": budjot._id,
+            "name": budjot.name,
+            "income": budjot.income,
+            "entries": budjotEntries
+        };
+    },
+    fromBudjotJson: function(budjot) {
+        // todo
     }
 };
 
