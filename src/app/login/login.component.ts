b3cfd150355406f01b2b427dec6eb0758007bd01
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-08 19:20:35 +0100

    Authentication complete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,26 +13,31 @@ export class LoginComponent implements OnInit {
     constructor(private authService: AuthService, private http: HttpClient) { }
 
     isLoggedIn: boolean;
-    accountCreated: boolean;
+    name: string;
 
     signInWithGoogle(): void {
         this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
     }
 
     signOut(): void {
-        this.authService.signOut();
+        if(this.isLoggedIn) {
+            this.authService.signOut();
+        }
     }
 
     onSignIn(user: any) {
-        if(user == null) { return; }
+        if(user == null) { 
+            this.isLoggedIn = false;
+            return; 
+        }
         var idToken = user.idToken;
+        this.name = user.name;
         
         var url = window.location.protocol + '//' + window.location.host + '/users';
 
         this.http.post(url, null, { headers: new HttpHeaders({'Content-Type': 'application/json','Authorization': idToken}), observe: 'response'}).subscribe(res => {
             console.log(res.status);
             if(res.status == 201) {
-                this.accountCreated = true;
                 this.isLoggedIn = true;
             } else if(res.status == 204) {
                 this.isLoggedIn = true;
