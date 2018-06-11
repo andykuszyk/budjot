21c828a868b3058bdc9bf1f840ad0d84355f2da7
andykuszyk <andy.kuszyk@carfinance247.co.uik>
2018-06-11 07:53:45 +0100

    Refactored auth into base class

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService, GoogleLoginProvider } from 'angularx-social-login';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AuthBase } from '../authbase';
+import { AuthService } from 'angularx-social-login';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -9,49 +9,8 @@ import { Injectable } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 @Injectable()
-export class LoginComponent implements OnInit {
-    constructor(private authService: AuthService, private http: HttpClient) { }
-
-    isLoggedIn: boolean;
-    name: string;
-
-    signInWithGoogle(): void {
-        this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+export class LoginComponent extends AuthBase {
+    constructor(http: HttpClient, authService: AuthService) {
+        super(http, authService);
     }
-
-    signOut(): void {
-        if(this.isLoggedIn) {
-            this.authService.signOut();
-        }
-    }
-
-    onSignIn(user: any) {
-        if(user == null) { 
-            this.isLoggedIn = false;
-            return; 
-        }
-        var idToken = user.idToken;
-        this.name = user.name;
-        
-        var url = window.location.protocol + '//' + window.location.host + '/users';
-
-        this.http.post(url, null, { headers: new HttpHeaders({'Content-Type': 'application/json','Authorization': idToken}), observe: 'response'}).subscribe(res => {
-            console.log(res.status);
-            if(res.status == 201) {
-                this.isLoggedIn = true;
-            } else if(res.status == 204) {
-                this.isLoggedIn = true;
-            } else {
-                console.log('Unknown response from /users: ' + res.status);
-            }
-        });
-    }
-
-    ngOnInit() {
-        this.authService.authState.subscribe((user) => { 
-            console.log(user);
-            this.onSignIn(user);
-        });
-    }
-
 }
