f1cce44ea09bc8f55a34899972f087a1ebb6e4b6
Andy Kuszyk <andykuszyk@live.co.uk>
2018-06-18 16:58:05 +0100

    Delete in progress

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Budjot } from '../budjot';
 import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-list',
@@ -27,7 +28,48 @@ export class ListComponent extends AuthBase {
         this.http.get(url, { headers: new HttpHeaders({'Content-Type': 'application/json', 'Authorization': this.idToken}), observe: 'response' }).subscribe(res => {
             console.log(res);
             this.budjots = res.body;
+            for(var budjot of this.budjots) {
+                budjot.isSelected = false;
+            }
         });
+    }
+
+    deleteSelectedBudjot() {
+        var budjot = null;
+        for(var b of this.budjots) {
+            if(b.isSelected) {
+                budjot = b;
+                break;
+            }
+        }
+        if(budjot == null) {
+            return;
+        }
+        console.log('Deleting budjot: ' + budjot.name);
+        var url = window.location.protocol + '//' + window.location.host + '/jots/' + budjot.id;
+        this.http.delete(url, { headers: new HttpHeaders({'Content-Type': 'application/json', 'Authorization': this.idToken}), observe: 'response' }).subscribe(res => {
+            
+        });
+    }
+
+    onBudjotSelected(budjot: any) {
+        for(var b of this.budjots) {
+            if(b.name != budjot.name) {
+                b.isSelected = false;
+            }
+        }
+    }
+
+    isBudjotSelected() {
+        if(this.budjots == null) {
+            return false;
+        }
+        for(var budjot of this.budjots) {
+            if(budjot.isSelected) {
+                return true;
+            }
+        }
+        return false;
     }
 
     openBudjot(budjot: any) {
