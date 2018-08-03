e0d78685b825c0d3206ed331daddeca08a649089
andykuszyk <andy.kuszyk@carfinance247.co.uik>
2018-08-03 20:46:23 +0100

    Tried to fix bug with mongo credentials

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,8 +2,8 @@
 commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
 docker build -t andykuszyk/budjot:$commit .
 docker push andykuszyk/budjot:$commit
-sed -i "s/\[MONGOUSER\]/$mongoUser/g" docker-compose.yml
-sed -i "s/\[MONGOPASSWORD\]/$mongoPassword/g" docker-compose.yml
+sed -i "s/\[mongouser\]/$mongoUser/g" docker-compose.yml
+sed -i "s/\[mongopassword\]/$mongoPassword/g" docker-compose.yml
 sed -i "s/\[version\]/$commit/g" docker-compose.yml
-scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
-ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk" 
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/budjot/docker-compose.yml
+ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/budjot/docker-compose.yml szyk" 
