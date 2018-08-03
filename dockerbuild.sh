1f6efb5e96be6d298c2874ff2fd7d6f5d40b0d3d
andykuszyk <andy.kuszyk@carfinance247.co.uik>
2018-08-03 20:59:31 +0100

    Moved compose directory back to home

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -5,5 +5,5 @@ docker push andykuszyk/budjot:$commit
 sed -i "s/\[mongouser\]/$mongoUser/g" docker-compose.yml
 sed -i "s/\[mongopassword\]/$mongoPassword/g" docker-compose.yml
 sed -i "s/\[version\]/$commit/g" docker-compose.yml
-scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/budjot/docker-compose.yml
-ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/budjot/docker-compose.yml szyk" 
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
+ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk" 
