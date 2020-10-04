9f3464a80be1c94ff21c2cc09319486ab7a461b6
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2020-10-04 21:54:42 +0100

    Moved over to do-docker deployment

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,3 +2,6 @@
 commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
 docker build -t andykuszyk/budjot:$commit .
 docker push andykuszyk/budjot:$commit
+git clone https://andykuszyk:$GITHUB_TOKEN@github.com/andykuszyk/do-docker
+cd do-docker
+make release-service SERVICE=budjot TAG=$commit
