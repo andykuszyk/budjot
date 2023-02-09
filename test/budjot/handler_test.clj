415ead4ebe8d5b22758a1144bc9a316cea36625d
andykuszyk <andy@kuszyk.com>
2023-02-09 19:56:45 +0000

    Added initial test cases for api contract

--- test/budjot/handler_test.clj
+++ test/budjot/handler_test.clj
@@ -3,12 +3,47 @@
             [ring.mock.request :as mock]
             [budjot.handler :refer :all]))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+(deftest get-jots
+  (let [response (app (mock/request :get "/jots"))]
+    (is (= (:status response) 200))
+    (is (= (:body response) "jots"))))
 
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+(deftest delete-jots-by-id
+  (let [response (app (mock/request :delete "/jots/1"))]
+    (is (= (:status response) 202))))
+
+(deftest get-jots-by-id
+  (let [response (app (mock/request :get "/jots/1"))]
+    (is (= (:status response) 200))))
+
+(deftest post-jots
+  (let [response (app (mock/request :post "/jots"))]
+    (is (= (:status response) 201))))
+
+(deftest put-jots-by-id
+  (let [response (app (mock/request :put "/jots/1"))]
+    (is (= (:status response) 200))))
+
+(deftest post-users
+  (let [response (app (mock/request :post "/users"))]
+    (is (= (:status response) 201))))
+
+(deftest get-root
+  (let [response (app (mock/request :get "/"))]
+    (is (= (:status response) 200))
+    (is (= (:body response) "html"))))
+
+(deftest get-login
+  (let [response (app (mock/request :get "/login"))]
+    (is (= (:status response) 200))
+    (is (= (:body response) "html"))))
+
+(deftest get-edit
+  (let [response (app (mock/request :get "/edit"))]
+    (is (= (:status response) 200))
+    (is (= (:body response) "html"))))
+
+(deftest get-list
+  (let [response (app (mock/request :get "/list"))]
+    (is (= (:status response) 200))
+    (is (= (:body response) "html"))))
