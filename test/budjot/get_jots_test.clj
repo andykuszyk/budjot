697369d1ff9d44c0b6f84dbe89c0999d6733437e
andykuszyk <andy@kuszyk.com>
2023-07-24 20:43:46 +0100

    Experimented with order, because GET seems to happen before POSTs 🤔

--- test/budjot/get_jots_test.clj
+++ test/budjot/get_jots_test.clj
@@ -18,9 +18,9 @@
       (is (= posted-jot got-jot)))))
 
 (deftest get-jots-returns-200-and-jots
-  (let [posted-jots (for [i (range 0 10)] (post-jot))]
-    (let [got-jots (json/read-str (:body (client/get (budjot-url))) :key-fn keyword)]
-      (is (= (count posted-jots) (count got-jots))))))
+  (let [got-jots (json/read-str (:body (client/get (budjot-url))) :key-fn keyword)
+       posted-jots (for [i (range 0 10)] (post-jot))]
+      (is (= posted-jots got-jots))))
 
 (deftest get-jot-doesnt-exist-returns-404
   (is (thrown-with-msg? Exception #"status 404" (client/get (format "%1s/6482284c0dc0525a1bb98786" (budjot-url))))))
