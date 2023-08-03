ab2d091758d30393d57304ce14b81ef52dce0efb
andykuszyk <andy@kuszyk.com>
2023-08-03 21:14:09 +0100

    Fixed lazy evaluation of `for` with `doall`

--- test/budjot/get_jots_test.clj
+++ test/budjot/get_jots_test.clj
@@ -13,14 +13,18 @@
   (json/read-str (:body (client/post (budjot-url) (post-request (jot (generate-name))))) :key-fn keyword))
 
 (deftest get-jot-by-id-returns-200-and-jot
-  (let [posted-jot (post-jot)]
-    (let [got-jot (json/read-str (:body (client/get (format "%1s/%2s" (budjot-url) (:_id posted-jot)))) :key-fn keyword)]
-      (is (= posted-jot got-jot)))))
+  (let [
+        posted-jot (post-jot)
+        got-jot (json/read-str (:body (client/get (format "%1s/%2s" (budjot-url) (:_id posted-jot)))) :key-fn keyword)
+        ]
+    (is (= posted-jot got-jot))))
 
 (deftest get-jots-returns-200-and-jots
-  (let [got-jots (json/read-str (:body (client/get (budjot-url))) :key-fn keyword)
-       posted-jots (for [i (range 0 10)] (post-jot))]
-      (is (= posted-jots got-jots))))
+  (let [
+        posted-jots (doall (for [i (range 0 10)] (post-jot)))
+        got-jots (json/read-str (:body (client/get (budjot-url))) :key-fn keyword)
+        ]
+    (is (= posted-jots got-jots))))
 
 (deftest get-jot-doesnt-exist-returns-404
   (is (thrown-with-msg? Exception #"status 404" (client/get (format "%1s/6482284c0dc0525a1bb98786" (budjot-url))))))
