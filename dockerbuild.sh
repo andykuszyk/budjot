4f568f336577b18617ead6ef391c1715306e0824
andykuszyk <andy@kuszyk.com>
2023-10-09 09:56:34 +0100

    Removed old ui related files for clean angular start

--- dockerbuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
-docker build -t andykuszyk/budjot:$commit .
-docker push andykuszyk/budjot:$commit
-git clone https://andykuszyk:$GITHUB_TOKEN@github.com/andykuszyk/do-docker
-cd do-docker
-make release-service SERVICE=budjot TAG=$commit
